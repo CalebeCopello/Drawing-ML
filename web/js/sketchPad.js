0bbe5e07aecae5fc5b7e52064e57a5cc3b503c45
CalebeCopello <114925816+CalebeCopello@users.noreply.github.com>
2023-08-03 19:53:33 -0300

    First part done

--- web/js/sketchPad.js
+++ web/js/sketchPad.js
@@ -9,13 +9,26 @@ class SketchPad{
         `
         container.appendChild(this.canvas)
 
+        const lineBreak=document.createElement("br")
+        container.appendChild(lineBreak)
+
+        this.undoBtn=document.createElement("button")
+        this.undoBtn.innerHTML="UNDO"
+        container.appendChild(this.undoBtn)
+
         this.ctx=this.canvas.getContext("2d")
 
-        this.paths=[]
-        this.isDrawing=false
+        this.reset()
 
         this.#addEventListeners()
     }
+
+    reset() {
+        this.paths=[]
+        this.isDrawing=false
+        this.#redraw()
+    }
+
     #addEventListeners(){
         this.canvas.onmousedown=(evt)=>{
             const mouse=this.#getMouse(evt)
@@ -30,14 +43,34 @@ class SketchPad{
                 this.#redraw()
             }
         }
-        this.canvas.onmouseup=()=>{
+        document.onmouseup=()=>{
             this.isDrawing=false
+        }
+        this.canvas.ontouchstart=(evt)=>{
+            const loc=evt.touches[0]
+            this.canvas.onmousedown(loc)
+        }
+        this.canvas.ontouchmove=(evt)=>{
+            const loc=evt.touches[0]
+            this.canvas.onmousemove(loc)
+        }
+        document.ontouchend=()=>{
+            document.onmouseup()
+        }
+        this.undoBtn.onclick=()=>{
+            this.paths.pop()
+            this.#redraw()
         }
     }
     #redraw(){
         this.ctx.clearRect(0,0,
             this.canvas.width,this.canvas.height)
         draw.paths(this.ctx,this.paths)
+        if(this.paths.length > 0) {
+            this.undoBtn.disabled=false
+        } else {
+            this.undoBtn.disabled=true
+        }
     }
     #getMouse=(evt)=>{
         const rect=this.canvas.getBoundingClientRect()
